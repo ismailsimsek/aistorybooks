35a07439fba2c76b82780c0d86c4061870f9d165
ismail simsek <6005685+ismailsimsek@users.noreply.github.com>
2025-03-25 20:17:34 +0100

    Create Classics2StoryBookGenerator using AutoGen (#3)

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 class Config:
-    GROQ_OPENAI_API_BASE = "https://api.groq.com/openai/v1"
+    GROQ_OPENAI_API_BASE_URL = "https://api.groq.com/openai/v1"
     GROQ_OPENAI_MODEL_NAME = "llama3-70b-8192"
     GROQ_OPENAI_API_KEY = Path(__file__).parent.joinpath("groq-api-key.txt").read_text().strip()
     OPENAI_API_KEY = Path(__file__).parent.joinpath("openai-api-key.txt").read_text().strip()
